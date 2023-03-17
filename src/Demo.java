6a18d2706a46f417cdf09129ffbec98e00a5e4fe
ankitakumari3 <ankitakumari3@USKOLANKITAKUM2.us.deloitte.com>
2023-03-17 15:39:28 +0530

    First commit...

---
+++ src/Demo.java
@@ -0,0 +1,17 @@
+
+public class Demo {
+
+
+	public static void main(String[] args)
+	{
+		String hungry="yes";
+		if(hungry=="yes")
+		{
+			System.out.print("eat");
+		}
+		else
+		{
+			System.out.print("work");
+		}
+	}
+}
